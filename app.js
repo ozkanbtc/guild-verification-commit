0cc906d0a3e6e147cd0cf5182415f295378eb2e7
Özkan <129770725+ozkanbtc@users.noreply.github.com>
2025-10-31 21:51:20 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-31T18:50:58.317Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2025-10-31T18:51:19.645Z
+Commit: Fix broken links in documentation
