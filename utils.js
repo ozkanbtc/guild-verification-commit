05526c45cf9cba365862ec841f40a57c9755f9bf
Özkan <129770725+ozkanbtc@users.noreply.github.com>
2025-10-31 21:49:53 +0300

    Introduce real-time notifications

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-31T18:49:44.962Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-10-31T18:49:52.813Z
+Commit: Introduce real-time notifications
