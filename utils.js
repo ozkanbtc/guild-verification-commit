cd925d69b0598115057acaec87f936e150b9d0b2
Özkan <129770725+ozkanbtc@users.noreply.github.com>
2025-10-31 21:51:01 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-31T18:50:49.702Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-10-31T18:51:00.365Z
+Commit: Resolve timezone issues in date handling
