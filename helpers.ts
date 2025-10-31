6a3517dc18c3b32c9e00416ab768fb8a86313e56
Özkan <129770725+ozkanbtc@users.noreply.github.com>
2025-10-31 21:51:25 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-31T18:51:16.465Z
-Commit: Implement user profile customization
+Timestamp: 2025-10-31T18:51:24.916Z
+Commit: Patch API endpoint error handling
